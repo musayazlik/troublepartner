b25a4e9786cad05678c260e210b71a6e45c9dea3
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-04-10 01:50:56 +0300

    .

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -266,7 +266,7 @@ const PostDetail = ({ session, data }) => {
                               key={commet._id}
                               className="card bg-slate-200 rounded-md p-4 border-2 border-slate-500/50 mb-3 last:mb-0 min-w-[400px]"
                             >
-                              <div className="flex justify-between items-center ">
+                              <div className="flex justify-between items-center gap-x-8 ">
                                 <div className="card_content w-full flex flex-col">
                                   <p
                                     className=""
