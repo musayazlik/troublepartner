65902ccde7564913146cb14e815254f56d82238c
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-05-02 15:58:47 +0300

    Düzeltme

--- pages/api/payment/callbackOkUrl.jsx
+++ pages/api/payment/callbackOkUrl.jsx
@@ -5,35 +5,29 @@ import dbConnect from "@/utils/dbconnect";
 export default async function handler(req, res) {
   await dbConnect();
 
-  console.log(req);
-
   try {
-    if (req.body.status === "success") {
-      const orderData = await Order.findOneAndUpdate(
-        { orderId: req.body.orderId },
-        {
-          paymentStatus: "paymentOk",
-          paymentType: req.body.paymentType,
-          valletOrderId: req.body.valletOrderId,
-          valletOrderNumber: req.body.valletOrderNumber,
-          paymentTime: req.body.paymentTime,
-        },
-        { new: true }
-      );
+    const orderData = await Order.findOneAndUpdate(
+      { orderId: req.body.orderId },
+      {
+        paymentStatus: "paymentOk",
+        paymentType: req.body.paymentType,
+        valletOrderId: req.body.valletOrderId,
+        valletOrderNumber: req.body.valletOrderNumber,
+        paymentTime: req.body.paymentTime,
+      },
+      { new: true }
+    );
 
-      await User.findOneAndUpdate(
-        { _id: orderData.user.id },
-        { memberType: "premium", premiumTime: Date.now() + 2592000000 },
-        { new: true }
-      );
+    await User.findOneAndUpdate(
+      { _id: orderData.user.id },
+      { memberType: "premium", premiumTime: Date.now() + 2592000000 },
+      { new: true }
+    );
 
-      res.writeHead(302, {
-        Location: "/payment/success",
-      });
-      res.end();
-    } else {
-      res.redirect(`${process.env.APP_URL}/payment/fail`);
-    }
+    res.writeHead(302, {
+      Location: "/payment/success",
+    });
+    res.end();
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
